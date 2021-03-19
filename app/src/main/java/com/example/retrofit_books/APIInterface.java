b43afd8d488e5ab3861abd10a7d66b96a15e5eee
arasgparsa.dev <arashparsa.dev@gmail.com>
2021-03-19 15:23:58 +0330

    payane fasl 2 retrofit - 19/3/2021

--- app/src/main/java/com/example/retrofit_books/APIInterface.java
+++ app/src/main/java/com/example/retrofit_books/APIInterface.java
@@ -1,11 +1,14 @@
 package com.example.retrofit_books;
 
 import com.example.retrofit_books.Model.Books;
+import com.example.retrofit_books.Model.Users;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 
 //ertebat beyne retrofit & server
@@ -14,4 +17,19 @@ public interface APIInterface {
 
     @GET("getdata.php")
     Call <List<Books>> getData() ;
+
+
+    @POST("register.php")
+    Call<Users> RegisterAccount(@Query("username") String username,
+                                @Query("email") String email,
+                                @Query("phone") String phone,
+                                @Query("password") String password);
+
+    @GET("login.php")
+    Call<Users> LoginAccount(@Query("email") String email,
+                             @Query("password") String password);
+
+
+
+
 }
